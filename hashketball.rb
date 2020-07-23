4b70315892f1cfbb8d321c16ae5eed7e1d04944a
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-23 17:57:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,13 @@
 def game_hash
 {
   :home => {
-    :team_name =>,
-    :colors =>,
+    :team_name =>
+    :colors =>
     :players =>
   }
   :away => {
-    :team_name =>,
-    :colors =>,
+    :team_name =>
+    :colors =>
     :players =>
 }
 end
