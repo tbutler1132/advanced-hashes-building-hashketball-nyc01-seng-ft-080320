97fde7afa9207b0b0e213b97c06df76a463a7b1b
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-23 18:15:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash
 {
   :home=>{
-    :team_name=>"Brooklyn Nets"
-    :colors=>["Black", "White"]
+    :team_name=>"Brooklyn Nets",
+    :colors=>["Black", "White"],
     :players=>[{
       :player_name=>"Alan Anderson",
       :number=>0,
