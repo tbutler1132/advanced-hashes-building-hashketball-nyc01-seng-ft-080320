636f2e339730814137292766049a62c9de530b4c
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-23 18:25:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,28 @@ def game_hash
     :team_name=>"Brooklyn Nets",
     :colors=>["Black", "White"],
     :players=>[{
-      :player_name=>"Alan Anderson",
-      :number=>0,
-      :shoe=>16,
-      :points=>22,
-      :rebounds=>12,
-      :assists=>12,
-      :steals=>3,
-      :blocks=>1,
-      :slam_dunks=>1
-      }]
+              :player_name=>"Alan Anderson",
+              :number=>0,
+              :shoe=>16,
+              :points=>22,
+              :rebounds=>12,
+              :assists=>12,
+              :steals=>3,
+              :blocks=>1,
+              :slam_dunks=>1
+      },
+        {
+              :player_name=>"Alan Anderson",
+              :number=>0,
+              :shoe=>16,
+              :points=>22,
+              :rebounds=>12,
+              :assists=>12,
+              :steals=>3,
+              :blocks=>1,
+              :slam_dunks=>1
+        }
+    ]
   },
   :away=>{
     :team_name=>"Charlotte Hornets",
