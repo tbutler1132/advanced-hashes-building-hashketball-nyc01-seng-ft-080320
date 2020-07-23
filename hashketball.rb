0f8bc5cdbdac9a72c00d572a687536d5a0fb3056
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-23 18:14:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,33 +1,33 @@
 def game_hash
 {
-  :home => {
-    :team_name => "Brooklyn Nets"
-    :colors => ["Black", "White"]
-    :players => [{
-      :player_name => "Alan Anderson",
-      :number => 0,
-      :shoe => 16,
-      :points => 22,
-      :rebounds => 12,
-      :assists => 12,
-      :steals => 3,
-      :blocks => 1,
-      :slam_dunks => 1,
+  :home=>{
+    :team_name=>"Brooklyn Nets"
+    :colors=>["Black", "White"]
+    :players=>[{
+      :player_name=>"Alan Anderson",
+      :number=>0,
+      :shoe=>16,
+      :points=>22,
+      :rebounds=>12,
+      :assists=>12,
+      :steals=>3,
+      :blocks=>1,
+      :slam_dunks=>1,
       }]
   }
-  :away => {
-    :team_name => "Charlotte Hornets"
-    :colors => ["Turquoise", "Purple"]
-    :players => [{
-      :player_name => "Jeff Adrien",
-      :number => 4,
-      :shoe => 18,
-      :points => 10,
-      :rebounds => 1,
-      :assists => 1,
-      :steals => 2,
-      :blocks => 7,
-      :slam_dunks => 2,
+  :away=>{
+    :team_name=>"Charlotte Hornets"
+    :colors=>["Turquoise", "Purple"]
+    :players=>[{
+      :player_name=>"Jeff Adrien",
+      :number=>4,
+      :shoe=>18,
+      :points=>10,
+      :rebounds=>1,
+      :assists=>1,
+      :steals=>2,
+      :blocks=>7,
+      :slam_dunks=>2,
       }]
 }
 end
