a4ab5237f79ae9d6b19730aefbd0974746d5b857
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-23 18:15:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,8 +16,8 @@ def game_hash
       }]
   }
   :away=>{
-    :team_name=>"Charlotte Hornets"
-    :colors=>["Turquoise", "Purple"]
+    :team_name=>"Charlotte Hornets",
+    :colors=>["Turquoise", "Purple"],
     :players=>[{
       :player_name=>"Jeff Adrien",
       :number=>4,
